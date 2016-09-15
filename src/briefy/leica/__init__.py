7da444e325d88a783007851d599eb2cffc19ec0a
Rudá Porto Filgueiras <rudazz@gmail.com>
2016-09-16 00:06:04 +0200

    Initialize briefy.ws with package version.

--- src/briefy/leica/__init__.py
+++ src/briefy/leica/__init__.py
@@ -10,17 +10,14 @@ from zope.configuration.xmlconfig import XMLConfig
 
 import briefy.ws
 import logging
+import pkg_resources
 
+__version__ = pkg_resources.get_distribution(__package__).version
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
 cs = logging.StreamHandler()
 cs.setLevel(logging.INFO)
-
 logger.addHandler(cs)
-
-
-logger = logging.getLogger(__name__)
 
 XMLConfig('configure.zcml', leica)()
 
@@ -41,5 +38,6 @@ def main(global_config, debug=False, **settings):
     config.registry['db_session_factory'] = Session
     config.add_request_method(get_db, 'db', reify=True)
     config.include('briefy.ws')
+    briefy.ws.initialize(config, version=__version__, project_name=__name__)
     config.scan()
     return config.make_wsgi_app()
