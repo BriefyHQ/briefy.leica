f36d0f78c9c022a982c5f2c1a00112bf3ed7e294
Érico Andrei <ericof@gmail.com>
2016-09-16 00:32:28 +0200

    More flake8

--- src/briefy/leica/storage/__init__.py
+++ src/briefy/leica/storage/__init__.py
@@ -1,13 +1,21 @@
+"""Storage adapters for briefy.leica."""
+
+
 class Storage:
+    """Storage."""
+
     def __init__(self, url, payload=None):
+        """Initialize storage."""
         self.url = url
         self.payload = payload
 
     def load(self):
+        """Load."""
         raise NotImplementedError
 
     def save(self):
+        """Save."""
         raise NotImplementedError
 
 
-from .s3 import S3Storage  #noQA
+from .s3 import S3Storage  # noQA
--- src/briefy/leica/storage/s3.py
+++ src/briefy/leica/storage/s3.py
@@ -1,10 +1,10 @@
-from briefy.leica.storage import Storage
-"""
-Code to store and retrieve image from Amazon S3
+"""Code to store and retrieve image from Amazon S3.
 
 To be done, as we are likely using thumbor for that.
-
 """
+from briefy.leica.storage import Storage
+
+
 class S3Storage(Storage):
     def load(self):
         pass
