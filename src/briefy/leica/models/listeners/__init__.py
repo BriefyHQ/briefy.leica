aa53efde37e10bd398ed8f7c4cd7b22724ed0391
Rudá Porto Filgueiras <rudazz@gmail.com>
2016-09-21 16:43:27 +0200

    Improve asset automatic transition.

--- src/briefy/leica/models/listeners/__init__.py
+++ src/briefy/leica/models/listeners/__init__.py
@@ -1,6 +1,8 @@
 """Sqlalchemy event listeners for briefy.leica models."""
 from briefy.common.workflow.exceptions import WorkflowPermissionException
 from briefy.leica import models
+from briefy.leica import logger
+from copy import copy
 from requests.exceptions import ConnectionError
 from sqlalchemy import event
 
@@ -14,18 +16,26 @@ def receive_asset_refresh_flush(target, flush_context, attrs):
     :param attrs:
     :return:
     """
-    user = target.request.user
-    workflow = target.workflow
-    workflow.context = user
-    if workflow.state.name == 'created':
-        try:
-            # automatic transition after created
-            workflow.submit()
-        except WorkflowPermissionException:
-            # TODO: logging
-            pass
+    request = (getattr(target, 'request', None))
+    if request is not None:
+        user = request.user
+        if user is not None:
+            user = request.user
+            workflow = target.workflow
+            workflow.context = user
+            if workflow.state.name == 'created':
+                try:
+                    workflow.submit()
+                    # TODO: create system user singleton and use hre
+                    system_user = copy(user)
+                    system_user.groups.append('g:system')
+                    workflow.context = user
+                    workflow.validate()
+                except WorkflowPermissionException:
+                    msg = 'Failure in automatic transition for asset: {id} title: {title}.'
+                    logger.debug(msg.format(id=target.id, title=target.title))
     try:
         target.update_metadata()
     except ConnectionError:
-        # TODO: logging
-        pass
+        msg = 'Failure updating metadata for asset: {id} title: {title}.'
+        logger.debug(msg.format(id=target.id, title=target.title))
