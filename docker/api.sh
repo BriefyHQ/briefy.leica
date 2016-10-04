583cfc9b0321c950b5ab460ade0b6198914b207a
Rudá Porto Filgueiras <rudazz@gmail.com>
2016-10-04 14:43:58 +0200

    Add absolute path to gunicorn.

--- docker/api.sh
+++ docker/api.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/docker_entrypoint.sh && NEW_RELIC_CONFIG_FILE=/app/newrelic.ini newrelic-admin run-python gunicorn --paste /app/configs/"${ENV}".ini
+/docker_entrypoint.sh && NEW_RELIC_CONFIG_FILE=/app/newrelic.ini newrelic-admin run-program /usr/local/bin/gunicorn --paste /app/configs/"${ENV}".ini
