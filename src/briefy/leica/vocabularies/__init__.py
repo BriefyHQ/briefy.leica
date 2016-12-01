0655fa6ec9087ac0ca73969e5d1c071d1020dd04
Érico Andrei <ericof@gmail.com>
2016-12-01 18:02:27 +0100

    Use new vocabulary format

--- src/briefy/leica/vocabularies/__init__.py
+++ src/briefy/leica/vocabularies/__init__.py
@@ -1,34 +1,62 @@
 """Vocabularies used by Leica."""
-from enum import Enum
+from briefy.common.vocabularies import LabeledEnum
 
 
-class ClientJobStatusChoices(Enum):
-    undefined = 'undefined'
-    job_received = 'Job received'
-    in_scheduling_process = 'In scheduling process'
-    scheduled = 'Scheduled'
-    in_qa_process = 'In QA process'
-    completed = 'Completed'
-    in_revision_ = 'In revision '
-    resolved = 'Resolved'
+options = [
+    ('undefined', 'undefined', 'undefined'),
+    ('job_received', 'job_received', 'Job received'),
+    ('in_scheduling_process', 'in_scheduling_process', 'In scheduling process'),
+    ('scheduled', 'scheduled', 'Scheduled'),
+    ('in_qa_process', 'in_qa_process', 'In QA process'),
+    ('completed', 'completed', 'Completed'),
+    ('in_revision_', 'in_revision_', 'In revision'),
+    ('resolved', 'resolved', 'Resolved'),
+]
 
 
-class SchedulingIssuesChoices(Enum):
-    undefined = 'undefined'
-    owner_not_responding = 'A1. Owner not responding'
-    rejected_by_owner_canceled = 'A2. Rejected by Owner / Canceled'
-    owner_not_being_able_to_give_exact_dates = 'A3. Owner not being able to give exact dates'
-    owner_not_aware_of_the_service = 'A4. Owner not aware of the service'
-    unable_to_schedule_due_to_property_unavailability = 'B1. Unable to schedule due to' \
-                                                        ' property unavailability (i.e. booked)'
-    property_undergoing_renovation = 'B2. Property undergoing renovation'
-    property_not_available_anymore = 'B3. Property not available anymore (i.e. sold)'
-    property_in_bad_shape = 'B4. Property in bad shape'
-    faulty_address_ = 'C1. Faulty address '
-    faulty_contact_details_ = 'C2. Faulty contact details '
-    weather_condition = 'C3. weather condition'
+ClientJobStatusChoices = LabeledEnum('ClientJobStatusChoices', options)
 
 
-class JobInputSource(Enum):
-    customer = 'customer'
-    briefy = 'briefy'
+options = [
+    ('undefined', 'undefined', 'undefined'),
+    ('owner_not_responding', 'owner_not_responding', 'A1. Owner not responding'),
+    (
+        'rejected_by_owner_canceled',
+        'rejected_by_owner_canceled',
+        'A2. Rejected by Owner / Canceled'
+    ),
+    (
+        'owner_not_being_able_to_give_exact_dates',
+        'owner_not_being_able_to_give_exact_dates',
+        'A3. Owner not being able to give exact dates'
+    ),
+    (
+        'owner_not_aware_of_the_service',
+        'owner_not_aware_of_the_service',
+        'A4. Owner not aware of the service'
+    ),
+    (
+        'property_unavailability',
+        'property_unavailability',
+        'B1. Unable to schedule due to property unavailability (i.e. booked)'
+    ),
+    ('undergoing_renovation', 'undergoing_renovation', 'B2. Property undergoing renovation'),
+    ('not_available_anymore', 'not_available_anymore',
+     'B3. Property not available anymore (i.e. sold)'),
+    ('in_bad_shape', 'in_bad_shape', 'B4. Property in bad shape'),
+    ('faulty_address', 'faulty_address', 'C1. Faulty address '),
+    ('faulty_contact_details', 'faulty_contact_details', 'C2. Faulty contact details '),
+    ('weather_condition', 'weather_condition', 'C3. weather condition'),
+]
+
+
+SchedulingIssuesChoices = LabeledEnum('SchedulingIssuesChoices', options)
+
+
+options = [
+    ('customer', 'customer', 'Input by Customer'),
+    ('briefy', 'briefy', 'Input by Briefy'),
+]
+
+
+JobInputSource = LabeledEnum('JobInputSource', options)
