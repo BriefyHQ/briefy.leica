3e0743580154e34a2e13ef8afd76633d77a6796e
Rudá Porto Filgueiras <rudaporto@users.noreply.github.com>
2016-12-21 10:06:43 +0100

    Include new description attribute on the search

--- src/briefy/leica/views/jobs.py
+++ src/briefy/leica/views/jobs.py
@@ -42,7 +42,7 @@ class JobService(RESTService):
     model = JobAssignment
     friendly_name = model.__name__
     default_order_by = 'created_at'
-    filter_related_fields = ['project.title', 'title']
+    filter_related_fields = ['project.title', 'title', 'description']
 
     _default_notify_events = {
         'POST': events.JobCreatedEvent,
