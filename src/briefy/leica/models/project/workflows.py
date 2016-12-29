05b1e72761ab8d4f4257e56fceb75e0efe7ec1f1
Érico Andrei <ericof@gmail.com>
2016-12-29 19:50:17 +0100

    Implement Project workflow

--- src/briefy/leica/models/project/workflows.py
+++ src/briefy/leica/models/project/workflows.py
@@ -1,5 +1,7 @@
 """Project Workflow."""
+from briefy.common.vocabularies.roles import Groups as G
 from briefy.common.workflow import BriefyWorkflow
+from briefy.common.workflow import Permission
 from briefy.common.workflow import WorkflowState as WS
 
 import logging
@@ -18,3 +20,35 @@ class ProjectWorkflow(BriefyWorkflow):
         'created', 'Created',
         'Project created.'
     )
+
+    ongoing = WS(
+        'ongoing', 'On Going',
+        'On Going.'
+    )
+
+    completed = WS(
+        'completed', 'Completed',
+        'Completed.'
+    )
+
+    # Transitions:
+    @created.transition(ongoing, 'can_start')
+    @completed.transition(ongoing, 'can_start')
+    def start(self):
+        """Start a project, moving it to On Going."""
+        pass
+
+    @ongoing.transition(completed, 'can_close')
+    def close(self):
+        """Close a project, moving it to Completed."""
+        pass
+
+    @Permission(groups=[G['bizdev'], G['system']])
+    def can_start(self):
+        """Validate if user can start this project."""
+        return True
+
+    @Permission(groups=[G['bizdev'], G['system']])
+    def can_close(self):
+        """Validate if user can close this project."""
+        return True
