ddf17b65dbf587487cd448646a30bfb5c31a3fe6
Érico Andrei <ericof@gmail.com>
2017-01-08 13:40:19 +0100

    Improve Customer workflow

--- src/briefy/leica/models/customer/workflows.py
+++ src/briefy/leica/models/customer/workflows.py
@@ -1,5 +1,8 @@
 """Customer Workflow."""
+from briefy.common.vocabularies.roles import Groups as G
+from briefy.common.vocabularies.roles import LocalRolesChoices as LR
 from briefy.common.workflow import BriefyWorkflow
+from briefy.common.workflow import Permission
 from briefy.common.workflow import WorkflowState as WS
 
 import logging
@@ -18,6 +21,54 @@ class CustomerWorkflow(BriefyWorkflow):
         'created', 'Created',
         'Customer created.'
     )
+
+    pending = WS(
+        'pending', 'Pending',
+        'A Customer that still needs to close a deal with Briefy.'
+    )
+
+    active = WS(
+        'active', 'Active',
+        'Active Customer.'
+    )
+
+    inactive = WS(
+        'inactive', 'Inactive',
+        'Inactive Customer.'
+    )
+
+    # Transitions:
+    @created.transition(pending, 'can_submit')
+    def submit(self):
+        """Submit a new customer to approval."""
+        pass
+
+    @pending.transition(active, 'can_activate')
+    @inactive.transition(active, 'can_activate')
+    def activate(self):
+        """Activate a customer."""
+        pass
+
+    @pending.transition(inactive, 'can_inactivate')
+    @active.transition(inactive, 'can_inactivate')
+    def inactivate(self):
+        """Inactivate a customer."""
+        pass
+
+    @Permission(groups=[LR['owner'], G['bizdev'], ])
+    def can_submit(self):
+        """Validate if user can submit a profile."""
+        return True
+
+    @Permission(groups=[LR['owner'], G['bizdev'], G['finance'] ])
+    def can_activate(self):
+        """Validate if user can activate this working location."""
+        return True
+
+    @Permission(groups=[LR['owner'], G['bizdev'], G['finance'] ])
+    def can_inactivate(self):
+        """Validate if user can inactivate this working location."""
+        return True
 
 
 class BillingAddressWorkflow(BriefyWorkflow):
