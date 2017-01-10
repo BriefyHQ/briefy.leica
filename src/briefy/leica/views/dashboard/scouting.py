53b00107079a00f6a907458fd2f9aa4c6df7b64f
Ruda Porto Filgueiras <rudazz@gmail.com>
2017-01-10 18:02:54 +0100

    Added Dashboard Scouting: Pool.

--- src/briefy/leica/views/dashboard/scouting.py
+++ src/briefy/leica/views/dashboard/scouting.py
@@ -1,4 +1,5 @@
 """Views to handle Scouting Dashboards creation."""
+from briefy.leica.models import Pool
 from briefy.leica.models.dashboard.scouting import DashboardScoutingCountry
 from briefy.leica.models.dashboard.scouting import DashboardScoutingProject
 from briefy.ws import CORS_POLICY
@@ -93,6 +94,52 @@ class DashboardScoutingProjectService(RESTService):
         ('assigned', {'label': 'Assigned Jobs', 'type': 'integer', 'url': '', 'filter': ''}),
         ('professionals',
          {'label': 'Active Photographers in Project', 'type': 'integer', 'url': '', 'filter': ''}
+         ),
+    )
+
+    @view(validators='_run_validators', permission='list')
+    def collection_get(self):
+        """Return a list of objects.
+
+        :returns: Payload with total records and list of objects
+        """
+        headers = self.request.response.headers
+        pagination = self.get_records()
+        total = pagination['total']
+        headers['Total-Records'] = '{total}'.format(total=total)
+        # Force in here to use the listing serialization.
+        pagination['data'] = [o.to_listing_dict() for o in pagination['data']]
+        if self.column_map:
+            pagination['columns'] = [{key: value} for key, value in self.column_map]
+        return pagination
+
+
+COLLECTION_PATH = '/dashboards/scouting/pool'
+PATH = COLLECTION_PATH + '/{id}'
+
+
+@resource(collection_path=COLLECTION_PATH,
+          path=PATH,
+          cors_policy=CORS_POLICY,
+          factory=DashboardScoutingFactory)
+class DashboardScoutingPoolService(RESTService):
+    """Dashboard Scouting: Pool Service."""
+
+    model = Pool
+    friendly_name = model.__name__
+    default_order_by = 'title'
+
+    column_map = (
+        ('pool', {'label': 'Pool', 'type': 'text', 'url': '', 'filter': ''}),
+        ('country', {'label': 'Country', 'type': 'country', 'url': '', 'filter': ''}),
+        ('total_assignments',
+         {'label': 'Total Assignments', 'type': 'integer', 'url': '', 'filter': ''}
+         ),
+        ('live_assignments',
+         {'label': 'Live Assignments', 'type': 'integer', 'url': '', 'filter': ''}
+         ),
+        ('total_professionals',
+         {'label': 'Number of Photographers', 'type': 'integer', 'url': '', 'filter': ''}
          ),
     )
 
