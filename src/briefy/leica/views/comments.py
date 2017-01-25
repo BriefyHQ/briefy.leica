3ea231d55396b29bfa7ff683384e33f18bb3cae0
Érico Andrei <ericof@gmail.com>
2017-01-25 13:12:00 +0100

    Remove unused import

--- src/briefy/leica/views/comments.py
+++ src/briefy/leica/views/comments.py
@@ -3,7 +3,6 @@ from briefy.leica.events import comment as events
 from briefy.leica.models import Comment
 from briefy.ws import CORS_POLICY
 from briefy.ws.resources import RESTService
-from briefy.ws.resources import WorkflowAwareResource
 from briefy.ws.resources.factory import BaseFactory
 from cornice.resource import resource
 from pyramid.security import Allow
