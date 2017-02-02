d4e8c4ea44367be650b68cd14381082902fa8205
Ruda Porto Filgueiras <rudazz@gmail.com>
2017-02-02 21:13:55 +0100

    Create new subscriber to do the UserProfile transition after creation. Also include workaround to set the customer ID again to avoid the case when the it was not possible to do this in the normal initialization and we need to do again after the flush.

---
+++ src/briefy/leica/subscribers/profile.py
@@ -0,0 +1,20 @@
+"""Event subscribers for briefy.leica.models.user.UserProfile."""
+from briefy.leica.events import userprofile as events
+from pyramid.events import subscriber
+
+
+@subscriber(events.UserProfileCreatedEvent)
+def userprofile_created_handler(event):
+    """Handle UserProfile created event."""
+    obj = event.obj
+    request = event.request
+
+    if obj.type == 'customeruserprofile':
+        # force this because sometimes the obj.id is not available before the flush
+        customer_roles = request.validated.get('customer_roles')
+        obj.customer_roles = customer_roles
+
+    # activate the user after creation
+    obj.workflow.activate()
+
+
