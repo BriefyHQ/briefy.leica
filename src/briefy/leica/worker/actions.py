f9c8ff732214a7e4ba5af8280757e61e3ab51c29
Érico Andrei <ericof@gmail.com>
2017-02-13 00:08:44 +0100

    Improve worker for Leica

--- src/briefy/leica/worker/actions.py
+++ src/briefy/leica/worker/actions.py
@@ -17,15 +17,24 @@ def validate_assignment(laure_data: object) -> (bool, dict):
     assignment = Assignment.query().get(assignment_id)
 
     if assignment.state != 'asset_validation':
-        logger.error('''Got message to validade assignemnt '{}' which is in state '{}' '''.format(
-                    assignment_id, assignment.state))
+        logger.error(
+            '''Got message to validade assignemnt '{0}' which is in state '{1}' '''.format(
+                assignment_id,
+                assignment.state
+            )
+        )
         return False, {}
 
-    logger.info('''Assignment '{}' assets reported as ok. Transitioning to 'in_qa' '''.format(
-                assignment_id))
+    logger.info(
+        '''Assignment '{0}' assets reported as ok. Transitioning to 'in_qa' '''.format(
+            assignment_id
+        )
+    )
     assignment.workflow_context = SystemUser
     with transaction.manager:
-        assignment.workflow.validate_assets()
+        assignment.workflow.validate_assets(
+            message='Validated submission.'
+        )
 
     return True, {}
 
@@ -40,12 +49,16 @@ def invalidate_assignment(laure_data: object) -> (bool, dict):
     assignment = Assignment.query().get(assignment_id)
 
     if assignment.state != 'asset_validation':
-        logger.error('''Got message to invalidate '{}' which is in state '{}' '''.format(
-                    assignment_id, assignment.state))
+        logger.error('''Got message to invalidate '{0}' which is in state '{1}' '''.format(
+            assignment_id,
+            assignment.state
+        ))
         return False, {}
 
-    feedback_text = '{0}\n\n{1}'.format(laure_data.validation_info.feedback,
-                                        laure_data.validation_info.complete_feedback)
+    feedback_text = '{0}\n\n{1}'.format(
+        laure_data.validation_info.feedback,
+        laure_data.validation_info.complete_feedback
+    )
 
     feedback_comment = Comment(author_id=SystemUser.id,
                                author_role='system',
@@ -53,7 +66,7 @@ def invalidate_assignment(laure_data: object) -> (bool, dict):
                                content=feedback_text)
 
     logger.info(
-        '''Assignment '{0}' assets reported as not suficient. Transitioning back '''
+        '''Assignment '{0}' assets reported as not sufficient. Transitioning back '''
         ''' to 'waiting_assets' and adding comments to assignment.'''.format(
             assignment_id
         )
@@ -63,7 +76,9 @@ def invalidate_assignment(laure_data: object) -> (bool, dict):
     assignment.workflow_context = SystemUser
     assignment.comments.append(feedback_comment)
     with transaction.manager:
-        assignment.workflow.invalidate_assets()
+        assignment.workflow.invalidate_assets(
+            message='Invalidated submission.'
+        )
 
     return True, {}
 
