ba3f9fc9f6c3c5c62c1ae9a11379f37ef6e1ab3c
Érico Andrei <ericof@gmail.com>
2017-02-14 17:35:11 +0100

    Small fixes

--- src/briefy/leica/worker/__init__.py
+++ src/briefy/leica/worker/__init__.py
@@ -58,7 +58,7 @@ MESSAGE_DISPATCH = {
 Dictionary for event dispatching. Expected format:
 each key is the event name itself - each value being another dictionary
 with the following keys:
- 'name': String withthe action name for purposes of logging and creating other messages
+ 'name': String with the action name for purposes of logging and creating other messages
  'action': A callable that will process the event 'objectified' payload,
            and return a tuple with a boolean and a dict the boolean indicates wether
            the action succeeded or failed; the dict is to be used
@@ -106,10 +106,13 @@ class Worker(QueueWorker):
 
         except Exception as error:
             status = False
-            logger.error('Unknown exception raised on \'{0}\' assignment {1}. Error: {2}'.format(
-                dispatch.name,
-                assignment.dct,
-                error))
+            logger.error(
+                'Unknown exception raised on \'{0}\' assignment {1}. Error: {2}'.format(
+                    dispatch.name,
+                    assignment.dct,
+                    error
+                )
+            )
             raise  # Let newrelic deal with it.
         event = None
         if status and dispatch.success_notification:
