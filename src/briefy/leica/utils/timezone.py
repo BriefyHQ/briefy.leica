ea119f23816438c193eee2eef0c30aa26b349050
Érico Andrei <ericof@gmail.com>
2017-02-15 15:25:01 +0100

    Add logging

--- src/briefy/leica/utils/timezone.py
+++ src/briefy/leica/utils/timezone.py
@@ -2,6 +2,8 @@
 
 Documentation: http://www.geonames.org/export/web-services.html
 """
+from briefy.leica import logger
+
 import requests
 
 ENDPOINT = 'http://api.geonames.org/timezoneJSON?formatted=true&lat={0}&lng={1}&username=briefy'
@@ -10,10 +12,22 @@ ENDPOINT = 'http://api.geonames.org/timezoneJSON?formatted=true&lat={0}&lng={1}&
 def timezone_from_coordinates(lat: float, lng: float) -> str:
     """Get timezone info from coordinates."""
     url = ENDPOINT.format(lat, lng)
-    r = requests.get(url)
-    data = r.json()
-    if data and data.get('timezoneId'):
-        return data['timezoneId']
-    elif data.get('status', {}).get('value') == 19:
-        # Rate limit
-        raise RuntimeError('Too many requests')
+    data = None
+    try:
+        r = requests.get(url)
+        data = r.json()
+        if data and data.get('timezoneId'):
+            return data['timezoneId']
+        elif data.get('status', {}).get('value') == 19:
+            # Rate limit
+            logger.error(
+                'Unable to fetch timezone: GeoNames rate limit.'
+            )
+        else:
+            logger.error(
+                'Unable to fetch timezone thanks to GeoNames throttling '
+            )
+    except Exception as exc:
+        logger.exception(
+            'Error retrieving timezone info: ' + str(exc)
+        )
