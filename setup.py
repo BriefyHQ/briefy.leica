b5f4747717f57fa9c54a0a295bd1374717f292c0
Ruda Porto Filgueiras <rudazz@gmail.com>
2017-02-20 18:10:54 +0100

    Bump version to 2.0.9 and add execute permission to tasks.sh docker script.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ test_requirements = [
 
 setup(
     name='briefy.leica',
-    version='2.0.8',
+    version='2.0.9',
     description='Image Handling System',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
