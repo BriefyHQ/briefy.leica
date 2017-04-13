9e91d43fc68d9d231d3aa249105397a328858a5c
Ruda Porto Filgueiras <rudazz@gmail.com>
2017-04-13 18:20:54 +0200

    Add migrations and improve update orders and assignments.

--- src/briefy/leica/tools/oneshots/20170413_update_from_history_orders_assignments.py
+++ src/briefy/leica/tools/oneshots/20170413_update_from_history_orders_assignments.py
@@ -7,32 +7,35 @@ from briefy.leica.tools import logger
 import transaction
 
 
-def update_from_history(model):
+def update_from_history(model, session):
     """Update from history all records from a given model."""
-    all = model.query().all()
-    logger.info('Will process {0} {1}'.format(len(all), model.__name__))
+    all = session.query(model.id).all()
+    name = model.__name__
+    logger.info('Will process {0} {1}'.format(len(all), name))
     for idx, item in enumerate(all):
         # History
+        item = model.get(item[0])
         item._update_dates_from_history()
+        logger.info('{0} {1} updated.'.format(name, idx))
+        if idx % 10 == 0:
+            transaction.commit()
 
     return idx
 
 
-def update_orders():
+def update_orders(session):
     """Update Order dates."""
-    with transaction.manager:
-        total = update_from_history(Order)
+    total = update_from_history(Order, session)
     logger.info('Finished processing {0} orders.'.format(total))
 
 
-def update_assignments():
+def update_assignments(session):
     """Update Assignments dates."""
-    with transaction.manager:
-        total = update_from_history(Assignment)
+    total = update_from_history(Assignment, session)
     logger.info('Finished processing {0} assignments.'.format(total))
 
 
 if __name__ == '__main__':
-    session = configure(Session)  # no
-    update_orders()
-    update_assignments()
+    session = configure(Session)
+    update_orders(session)
+    update_assignments(session)
