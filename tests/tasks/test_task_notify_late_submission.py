33c75eea66b5c50c9d0a5e0d132212180598c5b1
Ruda Porto Filgueiras <rudazz@gmail.com>
2017-07-17 14:06:40 +0200

    Added tests for notify late submission.

---
+++ tests/tasks/test_task_notify_late_submission.py
@@ -0,0 +1,61 @@
+"""Test Task to notify assignment late submission."""
+from briefy.leica import models
+from briefy.leica.tasks.assignment import _notify_late_submissions
+from briefy.leica.tasks.assignment import LATE_SUBMISSION_MSG
+from briefy.leica.tasks.assignment import notify_late_submissions
+from conftest import BaseTaskTest
+from datetime import timedelta
+
+import json
+
+
+class TestMoveAssignmentToAwaitingAssets(BaseTaskTest):
+    """Test Move Assignment to Awaiting Assets task."""
+
+    dependencies = [
+        (models.Professional, 'data/professionals.json'),
+        (models.Customer, 'data/customers.json'),
+        (models.Pool, 'data/jpools.json'),
+        (models.Project, 'data/projects.json'),
+        (models.Order, 'data/orders.json'),
+    ]
+
+    payload_position = -1
+    file_path = 'data/assignments.json'
+    model = models.Assignment
+
+    def test_wrong_state(self, instance_obj, now_utc):
+        """Will ignore assignment because its state and scheduled_datetime is not the expected one.
+
+        Assignment State is not awaiting assets.
+        """
+        assignment = instance_obj
+        assignment.scheduled_datetime = now_utc
+        notify_late_submissions()
+
+        messages = self.get_messages_from_queue()
+        assert len(messages) == 0
+
+    def test_success(self, instance_obj, now_utc):
+        """Will be successful if conditions are met.
+
+        Assignment State is scheduled
+        Assignment scheduled_datetime is in the past
+        """
+        assignment = instance_obj
+        assignment_id = assignment.id
+        assignment.state = 'awaiting_assets'
+        assignment.scheduled_datetime = now_utc - timedelta(seconds=49 * 3600)
+        status = _notify_late_submissions(assignment=assignment)
+
+        assert status is True
+        messages = self.get_messages_from_queue()
+        assert len(messages) == 1
+        for message in messages:
+            body = json.loads(message.body)
+            assert body['event_name'] in (
+                'leica.task.notify_late_submission.success'
+            )
+
+        assignment = models.Assignment.get(assignment_id)
+        assert assignment.comments[0].content == LATE_SUBMISSION_MSG
