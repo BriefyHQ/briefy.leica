4e001a5f7f85c119a492788d6b52faace98dbc9f
Ruda Porto Filgueiras <rudazz@gmail.com>
2017-11-13 09:11:47 +0100

    Change to use gunicorn + gevent in the development environment. This is important to manage multiple connections between the microservices, ex: briefy.reflex tasks.

--- docker/api.sh
+++ docker/api.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-/docker_entrypoint.sh && NEW_RELIC_CONFIG_FILE=/app/newrelic.ini newrelic-admin run-program pserve /app/configs/"${ENV}".ini
+if [ ${ENV}="development" ]
+then
+    COMMAND="gunicorn --paste"
+else
+    COMMAND="pserve"
+fi
+
+/docker_entrypoint.sh && NEW_RELIC_CONFIG_FILE=/app/newrelic.ini newrelic-admin run-program ${COMMAND} /app/configs/"${ENV}".ini
